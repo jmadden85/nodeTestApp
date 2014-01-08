ffaa466311aa10585a9b7febcbd635a44b8a2573
Joe Madden <joe@beardbro.com>
2014-01-08 11:16:54 -0500

    switched to mean stack

--- server.js
+++ server.js
@@ -1,37 +1,65 @@
+'use strict';
 
 /**
  * Module dependencies.
  */
+var express = require('express'),
+    fs = require('fs'),
+    passport = require('passport'),
+    logger = require('mean-logger');
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
+
+//Load configurations
+//Set the node enviornment variable if not set before
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//Initializing system variables 
+var config = require('./config/config'),
+    auth = require('./config/middlewares/authorization'),
+    mongoose = require('mongoose');
+
+//Bootstrap db connection
+var db = mongoose.connect(config.db);
+
+//Bootstrap models
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+    fs.readdirSync(path).forEach(function(file) {
+        var newPath = path + '/' + file;
+        var stat = fs.statSync(newPath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath);
+            }
+        } else if (stat.isDirectory()) {
+            walk(newPath);
+        }
+    });
+};
+walk(models_path);
+
+//bootstrap passport config
+require('./config/passport')(passport);
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-app.use(express.static(path.join(__dirname, 'public')));
+//express settings
+require('./config/express')(app, passport, db);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//Bootstrap routes
+require('./config/routes')(app, passport, auth);
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//Start the app by listening on <port>
+var port = process.env.PORT || config.port;
+app.listen(port);
+console.log('Express app started on port ' + port);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//Initializing logger
+logger.init(app, passport, mongoose);
+
+//expose app
+exports = module.exports = app;
